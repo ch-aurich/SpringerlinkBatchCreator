1eacb34e04f3eeaf81d71520bb95b8922763a73e
Christian Aurich <christian.aurich@c-aurich.de>
2010-11-21 21:07:36 +0000

    improved download and installing instructions in README and reworked the mainskript.

--- springerlinkBatchCreator.user.js
+++ springerlinkBatchCreator.user.js
@@ -2,7 +2,7 @@
 // @name           SpringerBatchloadListCreator
 // @namespace      http://c-aurich.de
 // @description    create a batchlist that is compatible to bakermans batchload for springer books
-// @include        http://www.springerlink.com/content/*
+// @include        http://*springerlink.com/*
 // ==/UserScript==
 
 
@@ -41,6 +41,7 @@ function getElementByClass(tagname, classname)
     for (var i = 0; i < allElements.length; i++) 
     {
         var thisElem = allElements[i];
+
         if (thisElem.className && thisElem.className == classname) 
         {
             return thisElem;
@@ -70,9 +71,10 @@ function cleanUpBatch()
     GM_setValue("SpringerCounter", newSpringerCounter);
     //alert("new count of Books: " + newSpringerCounter);
 
-    for (var i=0; i<j.length;i++)
+    for (var i=0; i<j;i++)
     {
-        GM_setValue("SpringerISBN" + i, Books[j]['ISBN']);
+        //alert(Books[i]['ISBN']);
+        GM_setValue("SpringerISBN" + (i + 1), Books[i]['ISBN']);
     }
 }
 
@@ -98,14 +100,15 @@ function delBookFromBatch()
 {
     var searchISBN = getRecentISBN();
     var SpringerCounter = GM_getValue("SpringerCounter", 0);
-
+    var success = false;
     for (var i=1; i<=SpringerCounter; i++)
     {
         recentISBN = GM_getValue("SpringerISBN" + i, "NULL");
-        if (searchISBN==recentISBN)
+        if (searchISBN==recentISBN&&success==false)
         {
             GM_deleteValue("SpringerISBN" + i);
             alert("deleted Book successfully");
+            success = true;
         }
     }
     cleanUpBatch();
@@ -118,7 +121,7 @@ function LinkDel2Add()
     delBookSpringerBatch.removeEventListener('click', delBookFromBatch, false); 
 
     elP = document.getElementById("SpringerBatchLink");
-    elP.innerHTML = "<a id=\"addBookSpringerBatch\">Buch zu Batch hinzufügen</a>";
+    elP.innerHTML = "<a id=\"addBookSpringerBatch\"><b>add book to batch</b></a><hr><hr>";
     
     var addBookSpringerBatch = document.getElementById('addBookSpringerBatch');
     addBookSpringerBatch.addEventListener('click', addBookToBatch, false);
@@ -147,7 +150,7 @@ function LinkAdd2Del()
     addBookSpringerBatch.removeEventListener('click', addBookToBatch, false); 
 
     elP = document.getElementById("SpringerBatchLink");
-    elP.innerHTML = "<a id=\"delBookSpringerBatch\">Buch von Batch entfernen</a>";
+    elP.innerHTML = "<a id=\"delBookSpringerBatch\"><b>delete book from batch</b></a><hr><hr>";
     
     var delBookSpringerBatch = document.getElementById('delBookSpringerBatch');
     delBookSpringerBatch.addEventListener('click', delBookFromBatch, false);
@@ -191,21 +194,40 @@ function isBookOnBatch()
 function SpringerBatchClear()
 {
     SpringerCounter = GM_getValue("SpringerCounter", 0);
-    var alertTXT = "Books added to Batch until now:\n";
+    var alertTXT = "books added to batch until now:\n";
     for (var i=1; i<=SpringerCounter; i++)
     {
         GM_deleteValue("SpringerISBN" + i, "NULL") + "\n";
     }
-    
     cleanUpBatch();
     updateBooksOnBatch();
+    updateDisplay();
 }
+
+function updateDisplay()
+{
+    if (isBookOnBatch()==0)
+    {
+        if (document.getElementById('SpringerBatchLink').innerHTML=="<a id=\"delBookSpringerBatch\"><b>delete book from batch</b></a><hr><hr>")
+        {
+            LinkDel2Add();
+        }
+    }
+    if (isBookOnBatch()==1)
+    {
+        if (document.getElementById('SpringerBatchLink').innerHTML=="<a id=\"addBookSpringerBatch\"><b>add book to batch</b></a><hr><hr>")
+        {
+            LinkAdd2Del();
+        }
+    }
+}
+
 
 
 function alertBooks()
 {
     SpringerCounter = GM_getValue("SpringerCounter", 0);
-    var alertTXT = "Books added to Batch until now:\n";
+    var alertTXT = "books added to batch until now:\n";
     for (var i=1; i<=SpringerCounter; i++)
     {
         alertTXT = alertTXT + GM_getValue("SpringerISBN" + i, "NULL") + "\n";
@@ -214,48 +236,54 @@ function alertBooks()
     alert(alertTXT);
 }
 
-var elAuthors, newElement;
+var elAuthors, newElement, isBookElement, isBook;
 
-
-elAuthors = getElementByClass("p", "authors");
-if (!elAuthors) elAuthors = getElementByClass("p", "editors");
-
-if (elAuthors) 
+isBook = false;
+isBookElement = getElementByClass("li", "first ui-state-default ui-corner-top ui-tabs-selected ui-state-active");
+if (isBookElement&&isBookElement.innerHTML=="<a href=\"#BookSection\">Book</a>")
 {
-    newElement = document.createElement('hr');
-    elAuthors.parentNode.insertBefore(newElement, elAuthors.nextSibling);
-    var newElement2;
-    newElement2 = document.createElement('p');
-    newElement2.id = "SpringerBatchLink";
-    newElement2.innerHTML = "<a id=\"addBookSpringerBatch\">Buch zu Batch hinzufügen</a>";
-    newElement.parentNode.insertBefore(newElement2, newElement.nextSibiling);
+    isBook = true;
+}
 
-    var addBookSpringerBatch = document.getElementById('addBookSpringerBatch');
-    addBookSpringerBatch.addEventListener('click', addBookToBatch, false);
-    
-    var elAbout = document.getElementById("AboutSection");
+var elAbout = document.getElementById("Cockpit");
+if (elAbout)
+{
     var newElement3;
     newElement3 = document.createElement('div');
     newElement3.id = "SpringerBatchListContainer";
     newElement3.setAttribute("class", "subsection");
-    newElement3.innerHTML = "<b>Books for batchdownload</b><div id=\"SpringerBatchList\"><p id=\"SpringerBatchNoBook\">no book so far</p></div><hr>\
+    newElement3.innerHTML = "<b id=\"booklistcaption\">books for batchdownload</b>\
+                            <div id=\"SpringerBatchList\"><p id=\"SpringerBatchNoBook\">no book so far</p></div><hr>\
                             <a id=\"SpringerBatchClear\">clear batch</a>";
     elAbout.parentNode.insertBefore(newElement3, elAbout.nextSibiling);
 
     var clearSpringerBatch = document.getElementById('SpringerBatchClear');
     clearSpringerBatch.addEventListener('click', SpringerBatchClear, false);
 
+    updateBooksOnBatch();
+}
 
+if (isBook) 
+{    
+    /*
+    elAuthors = getElementByClass("p", "authors");
+    if (!elAuthors) elAuthors = getElementByClass("p", "editors");
+    */
+    elBooklist = document.getElementById("booklistcaption");
+    newElement = document.createElement('p');
+    newElement.id = "SpringerBatchLink";
+    newElement.innerHTML = "<a id=\"addBookSpringerBatch\"><b>add book to batch</b></a><hr><hr>";
+
+    elBooklist.parentNode.insertBefore(newElement, elBooklist);
+
+    var addBookSpringerBatch = document.getElementById('addBookSpringerBatch');
+    addBookSpringerBatch.addEventListener('click', addBookToBatch, false);
+    
     if (isBookOnBatch()==1)
     {
         LinkAdd2Del();
     }
-
-
-
-    updateBooksOnBatch();
 }
-else
-{
-	alert("kein Buch?");
-}
+
+
+//unsafeWindow.addEventListener('onFocus', updateBooksOnBatch(), false);
